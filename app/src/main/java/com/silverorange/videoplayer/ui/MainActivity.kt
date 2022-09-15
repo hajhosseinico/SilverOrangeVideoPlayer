e122a0500d6b20b09adb6e27c05ef2b11ca66b59
Ali Hajhosseini <hajhosseinico@gmail.com>
2022-09-15 17:40:34 -0400

    Added toolbar, Changed Status bar color My Android studio was crashing so I upgraded Android Studio, SDK and etc so gradle-wrapper.properties, build.gradle and some other files have changed too Project tested all the implemented libraries work correctly

--- app/src/main/java/com/silverorange/videoplayer/ui/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.silverorange.videoplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.silverorange.videoplayer.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setStatusBarColor()
+    }
+
+    private fun setStatusBarColor(){
+        window.statusBarColor = ContextCompat.getColor(this, R.color.black)
     }
 }
